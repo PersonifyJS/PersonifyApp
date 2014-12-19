388ea0a4d20975292e6d15baf1f67b7f83641eaf
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-19 00:07:38 -0500

    Clean up package.json

--- node_modules/levelgraph/test/common.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-global.sinon = require('sinon');
-global.chai = require('chai');
-global.expect = require('chai').expect;
-global.chai.use(require('sinon-chai'));
-
-// polyfill setImmediate
-require('setimmediate');
