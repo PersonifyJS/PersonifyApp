926ccbdc26137755fa86d3ea6f667a7fca25d4a4
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-19 14:01:43 -0500

    pushed to activate codeship

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+//test
 var app = express();
 
 // view engine setup
